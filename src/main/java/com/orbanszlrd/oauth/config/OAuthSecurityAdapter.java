41a26e0e9722daa425bd8f54b71155d4d5eaf521
Orban Szilard <orbanszlrd@gmail.com>
2021-12-20 21:55:40 +0100

    Refactor the templates

--- src/main/java/com/orbanszlrd/oauth/config/OAuthSecurityAdapter.java
+++ src/main/java/com/orbanszlrd/oauth/config/OAuthSecurityAdapter.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
@@ -14,7 +15,7 @@ public class OAuthSecurityAdapter extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests(a -> a.antMatchers("/", "/error").permitAll().anyRequest().authenticated())
                 .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .csrf(c -> c.ignoringAntMatchers("/logout").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .logout(l -> l.logoutSuccessUrl("/").permitAll())
                 .oauth2Login();
     }
--- src/main/java/com/orbanszlrd/oauth/controller/IndexController.java
+++ src/main/java/com/orbanszlrd/oauth/controller/IndexController.java
@@ -5,16 +5,12 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
 public class IndexController {
     @GetMapping("/")
     public String index(@AuthenticationPrincipal OAuth2User user, Model model) {
-        System.out.println(user);
         model.addAttribute("user", user);
-
         return "index";
     }
 }
